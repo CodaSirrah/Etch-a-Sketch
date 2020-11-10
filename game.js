b5d16ab036c2be4ba78490fcb4fd62364f2325f6
codasirrah <harristurner97@gmail.com>
2020-11-10 18:19:52 +0000

    Problem with the greyScale function iterating the alpha value after another color has been selected. I acknowledge this issue and will hopefully solve it at a later date.

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ const mono = document.getElementById("mono");
 const greyScale = document.getElementById("greyScale");
 const rainbow = document.getElementById("rainbow");
 
+let monoSwitch = "off";
+let greyScaleSwitch = "on";
+let rainbowSwitch = "off";
+
+
 function createGrid(n, r) {
     gridParent.style.cssText = `grid-template-rows: repeat(${n}, minmax(0, ${100 / n}%)); 
     grid-template-columns: repeat(${n}, minmax(0, ${100 / n}));`;
@@ -22,33 +27,29 @@ function repeatCells(n) {
 };
 
 function fiftyShadesOfGrey(e) {
-    e.addEventListener("mouseover", () => {
-    let string;
-    string = e.style.backgroundColor.toString();
-    if (!(string.includes("rgb"))) return;
-    if (string.lastIndexOf("0") === 17 && !(string.lastIndexOf(".") === 18)) {
-    let index = string.lastIndexOf("0");
-        let num = string.slice("17", "18");
-        num = parseInt(num);
-        num += 0.1;
-        e.style.backgroundColor = `rgba(10,11,11, ${num})`;
-        string = e.style.backgroundColor.toString();
-    } else if (string.lastIndexOf(".") === 18) {
-        let decNum = string.slice("17", "20");
-        decNum = parseFloat(decNum);
-        decNum += 0.1;
-        e.style.backgroundColor = `rgba(10,11,11, ${decNum})`;
-        string = e.style.backgroundColor.toString();
-    }
-});
+    if (greyScaleSwitch ==="off") return;
+    else if (greyScaleSwitch === "on") {
+        e.addEventListener("mouseover", () => {
+        let string;
+        string = e.style.backgroundColor;
+        
+        if (!(string.includes("rgb"))) return;
+
+            let decNum = string.slice("17", "20");
+            decNum = parseFloat(decNum);
+            decNum += 0.1;
+            e.style.backgroundColor = `rgba(10,11,11, ${decNum})`;
+            string = e.style.backgroundColor;
+        if (!(string.includes("a"))) {
+            string = "rgba(10,11,11, 0.1";
+            e.style.background = string;
+            string = e.style.backgroundColor;
+            } 
+
+        });
+    };
 };
-
 window.onload = createGrid(16);
-
-const divs = document.querySelectorAll("div");
-divs.forEach((div) => {
-    fiftyShadesOfGrey(div);
-});
 
 function resetGrid(e) {
     reset.addEventListener("click", () => {
@@ -61,35 +62,90 @@ function resetGrid(e) {
             gridParent.removeChild(gridParent.lastChild);
         }
         createGrid(gridSize);
-        const divs = document.querySelectorAll("div");
-        divs.forEach((div) => {
-            fiftyShadesOfGrey(div);
-});
+        startDraw();
     });
 };
 reset.addEventListener("click", resetGrid());
 
 function colorRainbow(e) {
-    e.addEventListener("mouseover", () => {
-    string = e.style.backgroundColor.toString();
-    if (!(string.includes("rgb"))) return;
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
-    console.log(r, g, b);
-    // let newColor = `rgb(${r},${g},${b})`;
-    e.style.backgroundColor = `rgb(${r},${g},${b})`;
-    });
-}
-
+    if (rainbowSwitch === "off") return;
+    else if (rainbowSwitch === "on") {
+        e.addEventListener("mouseover", () => {
+            string = e.style.backgroundColor;
+            if (!(string.includes("rgb"))) return;
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            e.style.backgroundColor = `rgb(${r},${g},${b})`;
+        });
+    };
+};
 function colorMono(e) {
-    e.addEventListener("mouseover", () => {
-        string = e.style.backgroundColor.toString();
-        if (!(string.includes("rgb"))) return;
-        e.style.backgroundColor = "rgba(10,11,11, 0.5)";
-        
+    if (monoSwitch === "off") return;
+    else if (monoSwitch === "on") {
+        e.addEventListener("mouseover", () => {
+            string = e.style.backgroundColor;
+            if (!(string.includes("rgb"))) return;
+            e.style.backgroundColor = "rgb(129,144,148)";
+        });
+    };
+};
+greyScale.addEventListener("click", () => {
+    if (greyScaleSwitch === "off") {
+    greyScaleSwitch = "on";
+    console.log(`greyScaleSwitch: ${greyScaleSwitch}`);
+    monoSwitch = "off";
+    console.log(`monoSwitch: ${monoSwitch}`);
+    rainbowSwitch = "off";
+    console.log(`rainbowSwitch: ${rainbowSwitch}`);
+    mono.classList.remove("selected");
+    rainbow.classList.remove("selected");
+    greyScale.classList.add("selected");
+    startDraw();
+    }
+});
 
+mono.addEventListener("click", () => {
+    if (monoSwitch === "off") {
+    monoSwitch = "on";
+    console.log(`monoSwitch: ${monoSwitch}`);
+    greyScaleSwitch = "off"
+    console.log(`greyScaleSwitch: ${greyScaleSwitch}`);
+    rainbowSwitch = "off";
+    console.log(`rainbowSwitch: ${rainbowSwitch}`);
+    greyScale.classList.remove("selected");
+    rainbow.classList.remove("selected");
+    mono.classList.add("selected");
+    startDraw();
+    }
+});
+
+rainbow.addEventListener("click", () => {
+    if (rainbowSwitch === "off") {
+        rainbowSwitch = "on";
+        console.log(`rainbowSwitch: ${rainbowSwitch}`);
+        greyScaleSwitch = "off";
+        console.log(`greyScaleSwitch: ${greyScaleSwitch}`);
+        monoSwitch = "off";
+        console.log(`monoSwitch: ${monoSwitch}`);
+        greyScale.classList.remove("selected");
+        mono.classList.remove("selected");
+        rainbow.classList.add("selected");
+        startDraw();
+    }
+});
+
+function startDraw() {
+const divs = document.querySelectorAll("div");
+divs.forEach((div)  => {
+    if (greyScaleSwitch === "on") {
+        fiftyShadesOfGrey(div);
+    } else if (monoSwitch === "on") {
+        colorMono(div);
+    } else if (rainbowSwitch === "on") {
+        colorRainbow(div);
+    }
 });
 };
 
-mono.addEventListener("click", colorMono());
+startDraw();
